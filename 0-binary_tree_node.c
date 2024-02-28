13a8167ed56049d322c5b45fbc6fda23809c7217
MARWANANO32 <marwanano32@gmail.com>
2024-02-29 00:49:57 +0200

    edi readme

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/***
+ * binary_tree_node - creates a binary tree node.
+ *@parent: the pointer to print node.
+ *@value: the value to put in the new node.
+ * Return: pointer to a new node oterwise NULL.
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    
+}
