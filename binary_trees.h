39868163c4469077e4d54af4b59228a8e0d96acd
MARWANANO32 <marwanano32@gmail.com>
2024-03-01 19:50:23 +0200

    edi readme

--- binary_trees.h
+++ binary_trees.h
@@ -28,6 +28,11 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
 
 
 /*HELPER_FUNCTIONS*/
